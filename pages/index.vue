0d701ee92ec8f820676c83d58248c61d67b74233
Ariel <sonyt605@gmail.com>
2024-11-10 00:24:41 +0800

    add Bootstrap5

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,24 @@
 const route = useRoute();
 </script>
 <template>
-    <h1>
-      page: 首頁
-    </h1>
-    <p>目前路由的路徑 - route.fullPath 為 : {{route.fullPath}}</p>
-  </template>
-  
-  <style scoped></style>
+  <h1>page: 首頁</h1>
+  <p>目前路由的路徑 - route.fullPath 為 : {{ route.fullPath }}</p>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+
+  <button type="button" class="btn btn-link">Link</button>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: $primary;
+  line-height: $line-height-lg;
+}
+
+</style>
